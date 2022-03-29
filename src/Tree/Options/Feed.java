330685c6fff0fbfad852d6883a3a3739eee0e498
Tomas Uzcudun <tomas.uzcudun@students.salle.url.edu>
2022-03-29 11:47:31 +0200

    Codigo todo comentado y pull de codigo.

--- src/Tree/Options/Feed.java
+++ src/Tree/Options/Feed.java
@@ -11,11 +11,10 @@ public class Feed {
      * Constructor of the class feed to show all the nodes of the tree
      *
      * @param uiManagerTree class with all the messages and inputs to show.
-     * @param tree class with all the functions of the tree.
      *
      */
 
-    public Feed (UIManagerTree uiManagerTree, Tree tree) {
+    public Feed (UIManagerTree uiManagerTree) {
         this.uiManagerTree = uiManagerTree;
     }
 
@@ -24,7 +23,7 @@ public class Feed {
             return;
 
         inOrder(root.getRight());
-        uiManagerTree.printAlgorithm (root.getName(), root.getLanguage(), root.getCost(), tree.timestampToDate(root));
+        uiManagerTree.printAlgorithm (root.getName(), root.getLanguage(), root.getCost(), Tree.timestampToDate(root));
         inOrder(root.getLeft());
     }
 }
--- src/Tree/Options/SearchTimestamp.java
+++ src/Tree/Options/SearchTimestamp.java
@@ -12,18 +12,25 @@ public class SearchTimestamp {
      * Constructor of the search timestamp to do the exact search and the range search.
      *
      * @param uiManagerTree class with all the messages and inputs to show.
-     * @param tree class with all functions of the tree.
      *
      */
 
-    public SearchTimestamp (UIManagerTree uiManagerTree, Tree tree) {
+    public SearchTimestamp (UIManagerTree uiManagerTree) {
         this.uiManagerTree = uiManagerTree;
     }
+
+    /**
+     *
+     * Method to search the exact timestamp of a node.
+     *
+     * @param root tree with all the nodes.
+     * @param time timestamp that user want to search.
+     *
+     */
 
     public void searchExactTimestamp (Node root, long time) {
         if (root == null)
             return;
-
 
         if (time < root.getTimestamp()) {
             searchExactTimestamp (root.getLeft(), time);
@@ -37,17 +44,57 @@ public class SearchTimestamp {
 
     }
 
-    public void searchRangTimestamp (Node root, long timeStart, long timeEnd) {
-        if (root == null)
+    /**
+     *
+     * Method to search the timestamp that is in a specific range.
+     *
+     * @param node tree with all the nodes.
+     * @param min minimum timestamp to search
+     * @param max maximum timestamp to search
+     *
+     */
+
+    public void searchRangTimestamp (Node node, long min, long max) {
+        if (node == null)
             return;
 
-        searchRangTimestamp (root.getLeft(), timeStart, timeEnd);
-
-        if (root.getTimestamp() > timeStart && root.getTimestamp() < timeEnd) {
-            uiManagerTree.printRangTimestampAlgorithm(root.getName(), root.getLanguage(), root.getCost(), tree.timestampToDate(root));
+        if (max > node.getTimestamp()) {
+            searchRangTimestamp(node.getRight(), min, max);
         }
 
-        searchRangTimestamp (root.getRight(), timeStart, timeEnd);
+        if (node.getTimestamp() <= max && node.getTimestamp() >= min) {
+            uiManagerTree.printRangTimestampAlgorithm(node.getName(), node.getLanguage(), node.getCost(), Tree.timestampToDate(node));
+        }
+
+        searchRangTimestamp(node.getLeft(), min, max);
+    }
+
+    /**
+     *
+     * Method to count nodes that are in the range of the timestamp.
+     *
+     * @param node tree with all the nodes.
+     * @param min minimum number of the timestamp.
+     * @param max maximum number of the timestamp.
+     *
+     * @return the number of nodes.
+     *
+     */
+
+    public int countNodesInRange (Node node, long min, long max) {
+        if (node == null)
+            return countNodes;
+
+        if (max > node.getTimestamp()) {
+            countNodesInRange(node.getRight(), min, max);
+        }
+
+        if (node.getTimestamp() <= max && node.getTimestamp() >= min) {
+            countNodes++;
+        }
+
+        countNodesInRange(node.getLeft(), min, max);
+        return countNodes;
     }
 
 
